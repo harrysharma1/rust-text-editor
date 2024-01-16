aea06e5ed1b7462db86ad6b10cf3d6685de14a66
Harry Sharma <harrysharma1066@gmail.com>
2024-01-16 15:52:13 +0000

    update: print when typed rather than when pressed enter

--- luffy/src/main.rs
+++ luffy/src/main.rs
@@ -1,11 +1,16 @@
 use std::io;
 use std::io::Read;
+use std::io::stdout;
+use termion::raw::IntoRawMode;
+
+
 fn main() {
+    let _output = stdout().into_raw_mode().unwrap();
 
     for b in std::io::stdin().bytes() {
         let c = b.unwrap() as  char;
         println!("{}",c);
-        if c == 'w'{
+        if c == 'q'{
             break;
         }
     }
