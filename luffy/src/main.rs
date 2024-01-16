9da3be53976930590b447e4d490d0d2ff09fc351
Harry Sharma <harrysharma1066@gmail.com>
2024-01-16 16:13:25 +0000

    add: function to escape via ctrl+w

--- luffy/src/main.rs
+++ luffy/src/main.rs
@@ -4,20 +4,24 @@ use std::io::stdout;
 use termion::raw::IntoRawMode;
 
 
+fn ctrl_to_char(c:char)->u8{
+    let byte = c as u8;
+    byte & 0b00011111
+}
 fn main() {
     let _output = stdout().into_raw_mode().unwrap();
 
     for b in std::io::stdin().bytes() {
         let b: u8 = b.unwrap();
         let c: char = b as char;
-        println!("{}",c);
+       
         if c.is_control(){
-            print!("{:?}\r",b);
+            print!("{:?} \r",b);
         }else{
             println!("{:?} ({})\r", b, c);
         }
 
-        if c == 'q'{
+        if b == ctrl_to_char('w'){
             break;
         }
 
