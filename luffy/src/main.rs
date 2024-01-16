60163c3de22cf388634e01cff0cfc247ba0c7b51
Harry Sharma <harrysharma1066@gmail.com>
2024-01-16 15:41:28 +0000

    add: iterator looping through word to print char

--- luffy/src/main.rs
+++ luffy/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use std::io::Read;
 fn main() {
     println!("Hello, world!");
+
+    for b in std::io::stdin().bytes() {
+        let c = b.unwrap() as  char;
+        println!("{}",c);
+        if c == 'w'{
+            break;
+        }
+    }
 }
