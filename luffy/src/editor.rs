cb01a7ad8df60998546f69dcc5f7ae6d160fd6f6
Harry Sharma <harrysharma1066@gmail.com>
2024-01-16 17:02:41 +0000

    update: move logic to separate file

---
+++ luffy/src/editor.rs
@@ -0,0 +1,35 @@
+use std::io::{self,stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+    
+pub struct Editor{}
+
+
+impl Editor{
+    pub fn run(&self){
+        let _output = stdout().into_raw_mode().unwrap();
+        for key in io::stdin().keys(){
+            match key{
+                Ok(key)=> match key {
+                    Key::Char(c) =>{
+                        if c.is_control(){
+                            print!("{:?} \r",c as u8);
+                        }else{
+                            println!("{:?} ({})  \r", c as u8, c);
+                        }
+                    }
+                    Key::Ctrl('w') => break,
+                    _=> println!("{:?} \r", key)
+                }
+    
+                
+                Err(err) => error_handle(err)
+    
+            }
+        }
+    }
+}
+fn error_handle(e: std::io::Error){
+    panic!("{}", e);
+}
--- luffy/src/main.rs
+++ luffy/src/main.rs
@@ -1,34 +1,10 @@
-use std::io::{self,stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
 
+use editor::Editor; 
 
-
-
-fn error_handle(e: std::io::Error){
-    panic!("{}", e);
-}
 fn main() {
-    let _output = stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys(){
-        match key{
-            Ok(key)=> match key {
-                Key::Char(c) =>{
-                    if c.is_control(){
-                        print!("{:?} \r",c as u8);
-                    }else{
-                        println!("{:?} ({})  \r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('w') => break,
-                _=> println!("{:?} \r", key)
-            }
-
-            
-            Err(err) => error_handle(err)
-
-        }
-    }
+  
+  let editor = Editor{};
+  editor.run();
     
 }
