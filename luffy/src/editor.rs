9da583dfb2ebecf0ff0589a315312dc57f747a5b
Harry Sharma <harrysharma1066@gmail.com>
2024-01-17 14:36:23 +0000

    update: split read and write to separate functions

--- luffy/src/editor.rs
+++ luffy/src/editor.rs
@@ -1,39 +1,38 @@
-use std::io::{self,stdout};
+use std::io::{self, stdout};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-    
-pub struct Editor{}
 
+pub struct Editor {}
 
-impl Editor{
-    pub fn run(&self){
-        let _output = stdout().into_raw_mode().unwrap();
-        for key in io::stdin().keys(){
-            match key{
-                Ok(key)=> match key {
-                    Key::Char(c) =>{
-                        if c.is_control(){
-                            print!("{:?} \r",c as u8);
-                        }else{
-                            println!("{:?} ({})  \r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('w') => break,
-                    _=> println!("{key:?} \r")
-                }
-    
-                
-                Err(err) => error_handle(err)
-    
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        loop {
+            if let Err(error) = self.process_keypress() {
+                error_handle(error);
             }
         }
     }
-    pub fn default() -> Self{
-        Self{}
+    pub fn default() -> Self {
+        Self {}
+    }
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        if let Key::Ctrl('q') = pressed_key { panic!("Program end") }
+        Ok(())
     }
 }
 
-fn error_handle(e: std::io::Error){
-    panic!("{}", e);
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
+
+fn error_handle(e: std::io::Error) {
+    panic!("{}",e);
 }
